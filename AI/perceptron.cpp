d184e084254ce38525d62f72eae82eb6e88cb036
Josimar <jecs89@gmail.com>
2017-08-29 16:08:06 -0300

    Perceptron

---
+++ AI/perceptron.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <math.h>
+#include <random>
+#include "iomanip"
+#include <sstream>
+#include <algorithm>
+#include <thread>
+
+using namespace std;
+
+
+int main(int argc, char const *argv[]){
+
+	//AND
+	int pattern [4][3] = { 0, 1, 0, 
+						   0, 0, 0, 
+						   1, 0, 0, 
+						   1, 1, 1 };
+
+
+	/* code */
+	return 0;
+}
