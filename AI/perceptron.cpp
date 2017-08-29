d90257601ca1381db8c0c0b52fae61993b2e80a4
Josimar <jecs89@gmail.com>
2017-08-29 17:20:05 -0300

    And problem

--- AI/perceptron.cpp
+++ AI/perceptron.cpp
@@ -18,7 +18,7 @@ int sgn(double x){
 
 double my_sum( double x_1, double x_2, double bias, double y, vector<vector<double>> w ){
 	double val = x_1 * w[0][0] + x_2 * w[1][0] + bias * w[2][0];
-	cout << val << endl;
+//	cout << val << endl;
 	return sgn( val );
 }
 
@@ -26,16 +26,17 @@ double my_sum( double x_1, double x_2, double bias, double y, vector<vector<doub
 int main(int argc, char const *argv[]){
 
 	//AND
-	// int pattern [4][3] = { 0, 1, 0, 
-	// 					   0, 0, 0, 
-	// 					   1, 0, 0, 
-	// 					   1, 1, 1 };
+	double pattern [4][3] = { 0, 1, 0, 
+						   0, 0, 0, 
+						   1, 0, 0, 
+						   1, 1, 1 };
 
 
-	double pattern [4][3] = { 1.5, 3, 0, 
-						   3.5, 4, 1, 
-						   1.0, 2, 0, 
-						   4.0, 2, 1 };
+	//Artificial Pattern
+	// double pattern [4][3] = { 1.5, 3, 0, 
+	// 					   3.5, 4, 1, 
+	// 					   1.0, 2, 0, 
+	// 					   4.0, 2, 1 };
 
 
 	vector<vector<double>> W_1 ( 3, vector<double>(1));
@@ -46,54 +47,45 @@ int main(int argc, char const *argv[]){
 	uniform_real_distribution<double> dist( 0, 1 ); 
 	cout << "initial weigths\n";
 
-	W_1[0][0] = 0.5;
-	W_1[1][0] = -0.1;
-	W_1[2][0] = 0.2;
+	// W_1[0][0] = 0.5;
+	// W_1[1][0] = -0.1;
+	// W_1[2][0] = 0.2;
 
 	for (int i = 0; i < 3; ++i){
-	//	W_1[i][0] = dist(rng);
+		W_1[i][0] = dist(rng);
 		cout << W_1[i][0] << "\t";
 	}
 	cout << endl;
 
-	double n = 0.9;
+	double n = 0.5;
 
 	double sum = 0.0;
 
-	for (int i = 0; i < 4; ++i){
-		cout << "pattern " << i << endl;
+	int maxIter = 20;
+	int iter = 0;
 
-		sum = my_sum( pattern[i][0], pattern[i][1], bias, pattern[i][2], W_1 );
-		cout << "sum: " << sum << "vs" << pattern[i][2] << endl;
 
-		double my_error;
-		if( sum != pattern[i][2] ){
-			my_error = ( pattern[i][2] - sum );
-			cout << "--" << my_error << endl;
-			W_1[0][0] = W_1[0][0] + n * pattern[i][0] * my_error;
-			W_1[1][0] = W_1[1][0] + n * pattern[i][1] * my_error;
-			W_1[2][0] = W_1[2][0] + n * bias * my_error;
-		}
-		cout << "weigths " << setw(8) << W_1[0][0] << setw(8) << W_1[1][0] <<  setw(8) << W_1[2][0] << setw(8) << my_error << endl;
-	}
+	while( iter < maxIter ){
+		cout << "Iteration " << iter << endl;
 
 		for (int i = 0; i < 4; ++i){
-		cout << "pattern " << i << endl;
+			//cout << "pattern " << i << endl;
 
-		sum = my_sum( pattern[i][0], pattern[i][1], bias, pattern[i][2], W_1 );
-		cout << "sum: " << sum << "vs" << pattern[i][2] << endl;
+			sum = my_sum( pattern[i][0], pattern[i][1], bias, pattern[i][2], W_1 );
+			//cout << "sum: " << sum << "vs" << pattern[i][2] << endl;
 
-		double my_error;
-		if( sum != pattern[i][2] ){
-			my_error = ( pattern[i][2] - sum );
-			cout << "--" << my_error << endl;
-			W_1[0][0] = W_1[0][0] + n * pattern[i][0] * my_error;
-			W_1[1][0] = W_1[1][0] + n * pattern[i][1] * my_error;
-			W_1[2][0] = W_1[2][0] + n * bias * my_error;
+			double my_error;
+			if( sum != pattern[i][2] ){
+				my_error = ( pattern[i][2] - sum );
+				//cout << "--" << my_error << endl;
+				W_1[0][0] = W_1[0][0] + n * pattern[i][0] * my_error;
+				W_1[1][0] = W_1[1][0] + n * pattern[i][1] * my_error;
+				W_1[2][0] = W_1[2][0] + n * bias * my_error;
+			}
+			cout << "weigths " << setw(10) << W_1[0][0] << setw(10) << W_1[1][0] << setw(10) << W_1[2][0] << setw(4) << my_error << endl;
 		}
-		cout << "weigths " << setw(8) << W_1[0][0] << setw(8) << W_1[1][0] <<  setw(8) << W_1[2][0] << setw(8) << my_error << endl;
+		iter++;
 	}
-
 
 	/* code */
 	return 0;
