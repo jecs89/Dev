3fcdb749ae700b2e53ef7e9fe41cc865f4fb3d1e
Josimar <jecs89@gmail.com>
2017-08-31 15:52:30 -0300

    Hidden, Output Indexes Fixed

--- AI/multilayer.cpp
+++ AI/multilayer.cpp
@@ -40,11 +40,17 @@ int main(int argc, char const *argv[]){
 	//reading_params(name,params,patterns);
 
 	//AND
-	double pattern [4][3] = { 0, 1, 0, 
+	/*double pattern [4][3] = { 0, 1, 0, 
 						   0, 0, 0, 
 						   1, 0, 0, 
 						   1, 1, 1 };
+*/
 
+
+	int D = 2;
+	int O = 2;
+
+	double pattern [1][4] = { 0.05, 0.1, 0.01, 0.99};
 
 	//Artificial Pattern
 	// double pattern [4][3] = { 1.5, 3, 0, 
@@ -53,7 +59,7 @@ int main(int argc, char const *argv[]){
 	// 					   4.0, 2, 1 };
 
 
-	vector<vector<double>> W ( 3, vector<double>(2));
+	vector<vector<double>> W ( 5, vector<double>(2));
 	vector<vector<double>> bias (3, vector<double>(2));
 
 	//init
@@ -61,15 +67,29 @@ int main(int argc, char const *argv[]){
 	uniform_real_distribution<double> dist( 0, 1 ); 
 	cout << "initial weigths\n";
 
+	W[0][0] = .15;
+	W[1][0] = .20;
+	W[2][0] = .25;
+	W[3][0] = .30;
+	W[4][0] = .35;
+	 
+	W[0][1] = .40;
+	W[1][1] = .45;
+	W[2][1] = .50;
+	W[3][1] = .55;
+	W[4][1] = .60;
+
+	/*
 	for (int i = 0; i < 3; ++i){
 		for(int j = 0 ; j < W[0].size() ; j++){
-			W[i][j] = dist(rng);
+			//W[i][j] = dist(rng);
+			//W[i][j] = 0.5;
 			bias[i][j] = 1.0;
 			cout << W[i][j] << "\t";
 		}
 		cout << endl;
 	}
-	
+	*/
 
 	double n = 0.5;
 
@@ -78,42 +98,59 @@ int main(int argc, char const *argv[]){
 	int maxIter = 1;
 	int iter = 0;
 
-	int n_hidden = 3;
+	int n_hidden = D;
 
 	vector<double> z( n_hidden );
 
-	int n_out = 1;
+	int n_out = O;
 	vector<double> y( n_out );
 
 
 	while( iter < maxIter ){
 		cout << "Iteration " << iter << endl;
 
-	 	for (int i = 0; i < 4; ++i){
+	 	for (int i = 0; i < 1; ++i){
 	 		cout << "pattern " << i << endl;
 
 	 		//sum = my_sum( pattern[i][0], pattern[i][1], bias, pattern[i][2], W_1 );
+
+	 		int i_w = 0;
+	 		int i_w_lim = D;
 
 	 		//Sum for hidden neurons
 	 		for( int i_z = 0 ; i_z < z.size() ; i_z++ ){
 	 			cout << "h neuron " << i_z << endl;
 	 			z[i_z] = 0;
-	 			for( int i_p = 0 ; i_p < 2 ; i_p++ ){
-	 				z[i_z] += pattern[i][i_p]*W[i_z][0];
-	 			}
-	 			z[i_z] += 1.0*W[2][0];
 
-	 			cout << z[i_z] << endl;
+	 			for( int i_p = 0 ; i_p < D ; i_p++  ){
+	 				z[i_z] += pattern[i][i_p]*W[i_w][0];
+	 				cout << pattern[i][i_p] << "*" << W[i_w][0] << "=" << pattern[i][i_p]*W[i_w][0] << endl;
+	 				i_w++;
+	 			}
+	 			z[i_z] += 1.0*W[4][0];
+
+	 			i_w = D;
+	 			i_w_lim = (i_z+2)*D;
+
+	 			cout << z[i_z] << "\t" << sigmoid(z[i_z]) << endl;
 	 		}
+
+	 		i_w = 0;
+	 		i_w_lim = n_hidden;
 
 	 		//Sum for outputs neurons
 	 		for( int i_o = 0 ; i_o < y.size() ; i_o++ ){
 	 			cout << "o neuron " << i_o << endl;
 	 			y[i_o] = 0;
 	 			for( int i_h = 0 ; i_h < z.size() ; i_h++ ){
-	 				y[i_o] += z[i_h]*W[i_h][1];
+	 				y[i_o] += sigmoid(z[i_h])*W[i_w][1];
+	 				cout << sigmoid(z[i_h]) << "*" << W[i_w][1] << "=" << sigmoid(z[i_h])*W[i_w][1] << endl;
+	 				i_w++;
 	 			}
-	 			y[i_o] += 1.0*W[2][1];
+	 			y[i_o] += 1.0*W[4][1];
+
+	 			i_w = D;
+	 			i_w_lim = (i_o+2)*n_hidden;
 
 	 			cout << y[i_o] << endl;
 	 		}
